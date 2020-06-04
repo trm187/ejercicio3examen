fdb971aaf39be21e2253f24c8650d5661a82054a
Tesi <trm187@inlumine.ual.es>
2020-06-04 19:52:44 +0200

    arreglos

--- ejercicio3Examen/test/ejercicio3Examen/TestEjercicio3Examen.java
+++ ejercicio3Examen/test/ejercicio3Examen/TestEjercicio3Examen.java
@@ -1,6 +1,7 @@
 package ejercicio3Examen;
 
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
